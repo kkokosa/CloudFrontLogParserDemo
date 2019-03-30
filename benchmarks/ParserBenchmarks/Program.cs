cf3eec5077b21103ab6d7e96b063bfe13b0f2fbe
Steve Gordon <sgordon@hotmail.co.uk>
2019-03-30 15:54:41 +0000

    Adding benchmarks

---
+++ benchmarks/ParserBenchmarks/Program.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using CloudFrontLogParserDemo;
+using System;
+using System.Buffers;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ParserBenchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<CloudFrontParserBenchmarks>();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class CloudFrontParserBenchmarks
+    {
+        [Benchmark(Baseline = true)]
+        public async Task Original()
+        {
+            var directoryPath = Path.GetDirectoryName(Assembly.GetAssembly(typeof(CloudFrontRecord)).Location);
+            var filePath = Path.Combine(directoryPath, "sample-cloudfront-access-logs.gz");
+
+            for (int i = 0; i < 5; i++)
+            {
+                var results = await OldCloudFrontParser.ParseAsync(filePath);
+            }
+        }
+
+        [Benchmark]
+        public async Task Optimised()
+        {
+            var directoryPath = Path.GetDirectoryName(Assembly.GetAssembly(typeof(CloudFrontRecord)).Location);
+            var filePath = Path.Combine(directoryPath, "sample-cloudfront-access-logs.gz");
+
+            var newData = ArrayPool<CloudFrontRecord>.Shared.Rent(100); // make the record a struct?
+
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    await NewCloudFrontParser.ParseAsync(filePath, newData);
+                }
+                finally
+                {
+                    ArrayPool<CloudFrontRecord>.Shared.Return(newData, clearArray: true);
+                }
+            }           
+        }
+    }
+}
--- src/CloudFrontLogParserDemo/Program.cs
+++ src/CloudFrontLogParserDemo/Program.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
-using System.Linq;
+﻿using System.IO;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
-using TinyCsvParser;
-using TinyCsvParser.Mapping;
-using Pipelines.Sockets.Unofficial;
 using System.Buffers;
 
 namespace CloudFrontLogParserDemo
@@ -18,236 +10,24 @@ namespace CloudFrontLogParserDemo
         static async Task Main()
         {
             var directoryPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-
             var filePath = Path.Combine(directoryPath, "sample-cloudfront-access-logs.gz");
 
-            if (File.Exists(filePath))
+            // OLD
+
+            var originalData = await OldCloudFrontParser.ParseAsync(filePath);
+
+            // NEW
+
+            var newData = ArrayPool<CloudFrontRecord>.Shared.Rent(100); // make the record a struct?
+
+            try
             {
-                using (var fileStream = File.OpenRead(filePath))
-                using (var decompressionStream = new GZipStream(fileStream, CompressionMode.Decompress))
-                using (var decompressedStream = new MemoryStream())
-                {
-                    await decompressionStream.CopyToAsync(decompressedStream);
-
-                    var data = Encoding.UTF8.GetString(decompressedStream.ToArray());
-
-                    if (!string.IsNullOrEmpty(data))
-                    {
-                        var parser = new CloudWatchLogParser();
-                        var parsedData = parser.Parse(data);
-                    }
-                }
-
-                // new
-
-                var items = ArrayPool<CsvEmailBeaconLogRecord>.Shared.Rent(100); // make the record a struct?
-
-                try
-                {
-                    using (var fileStream = File.OpenRead(filePath))
-                    using (var decompressionStream = new GZipStream(fileStream, CompressionMode.Decompress))
-                    {
-                        var pipeReader = StreamConnection.GetReader(decompressionStream);
-
-                        var position = 0;
-
-                        while (true)
-                        {
-                            var result = await pipeReader.ReadAsync();
-
-                            var buffer = result.Buffer;
-
-                            ParseLines(items, ref buffer, ref position);
-
-                            // Tell the PipeReader how much of the buffer we have consumed
-                            pipeReader.AdvanceTo(buffer.Start, buffer.End);
-
-                            // Stop reading if there's no more data coming
-                            if (result.IsCompleted)
-                            {
-                                break;
-                            }
-                        }
-
-                        // Mark the PipeReader as complete
-                        pipeReader.Complete();
-                    }
-                }
-                finally
-                {
-                    ArrayPool<CsvEmailBeaconLogRecord>.Shared.Return(items, clearArray: true);
-                }
+                await NewCloudFrontParser.ParseAsync(filePath, newData);
             }
-        }
-
-        private static void ParseLines(CsvEmailBeaconLogRecord[] itemsArray, ref ReadOnlySequence<byte> buffer, ref int position)
-        {
-            const byte newLine = (byte)'\n';
-
-            var reader = new SequenceReader<byte>(buffer);
-
-            while (!reader.End)
+            finally
             {
-                var span = reader.UnreadSpan;
-                var index = span.IndexOf(newLine);
-                int length;
-
-                if (index != -1)
-                {
-                    length = index;
-
-                    var parsedLine = LineParser.ParseLine(span.Slice(0, index));
-
-                    if (parsedLine != null) itemsArray[position++] = parsedLine;
-                }
-                else
-                {
-                    // We didn't find the new line in the current segment, see if it's 
-                    // another segment
-                    var current = reader.Position;
-                    var linePos = buffer.Slice(current).PositionOf(newLine);
-
-                    if (linePos == null)
-                    {
-                        // Nope
-                        break;
-                    }
-
-                    // We found one, so get the line and parse it
-                    var line = buffer.Slice(current, linePos.Value);
-
-                    var parsedLine = ParseLine(line);
-
-                    if (parsedLine != null) itemsArray[position++] = parsedLine;
-
-                    length = (int)line.Length;
-                }
-
-                // Advance past the line + the \n
-                reader.Advance(length + 1);
-            }
-
-            // Update the buffer
-            buffer = buffer.Slice(reader.Position);
-        }
-
-        private static CsvEmailBeaconLogRecord ParseLine(in ReadOnlySequence<byte> line)
-        {
-            // Lines are always small so we incur a small copy if we happen to cross a buffer boundary
-            if (line.IsSingleSegment)
-            {
-                return LineParser.ParseLine(line.First.Span);
-            }
-            else if (line.Length < 256)
-            {
-                // Small lines we copy to the stack
-                Span<byte> stackLine = stackalloc byte[(int)line.Length];
-                line.CopyTo(stackLine);
-                return LineParser.ParseLine(stackLine);
-            }
-            else
-            {
-                // Should be extremely rare
-                var length = (int)line.Length;
-                var buffer = ArrayPool<byte>.Shared.Rent(length);
-                line.CopyTo(buffer);
-                var emailBeaconCloudWatchLogRecord = LineParser.ParseLine(buffer.AsSpan(0, length));
-                ArrayPool<byte>.Shared.Return(buffer);
-                return emailBeaconCloudWatchLogRecord;
-            }
-        }
-
-        internal static class LineParser
-        {
-            private const byte Tab = (byte)'\t', Hash = (byte)'#';
-
-            public static CsvEmailBeaconLogRecord ParseLine(ReadOnlySpan<byte> line)
-            {
-                if (line[0] == Hash) return null;
-
-                var tabCount = 1;
-
-                var record = new CsvEmailBeaconLogRecord();
-
-                while (tabCount < 13)
-                {
-                    var tabAt = line.IndexOf(Tab);
-
-                    if (tabCount == 1)
-                    {
-                        {
-                            var value = Encoding.UTF8.GetString(line.Slice(0, tabAt));
-                            record.Date = value;
-                        }
-                    }
-                    else if (tabCount == 2)
-                    {
-                        {
-                            var value = Encoding.UTF8.GetString(line.Slice(0, tabAt));
-                            record.Time = value;
-                        }
-                    }
-                    else if (tabCount == 11)
-                    {
-                        {
-                            var value = Encoding.UTF8.GetString(line.Slice(0, tabAt));
-                            record.UserAgent = value;
-                        }
-                    }
-
-                    line = line.Slice(tabAt + 1);
-
-                    tabCount++;
-                }
-
-                return record;
-            }
-        }
-
-        public class CsvEmailBeaconLogRecord
-        {
-            public string Date { get; set; }
-            public string Time { get; set; }
-            public string UserAgent { get; set; }
-        }
-
-        public class CloudWatchLogParser
-        {
-            private readonly CsvParser<CsvEmailBeaconLogRecord> _csvParser;
-
-            public CloudWatchLogParser()
-            {
-                var csvParserOptions = new CsvParserOptions(true, "#", new TinyCsvParser.Tokenizer.RFC4180.RFC4180Tokenizer(new TinyCsvParser.Tokenizer.RFC4180.Options('"', '\\', '\t')));
-                var csvMapper = new EmailBeaconLogRecordMapping();
-
-                _csvParser = new CsvParser<CsvEmailBeaconLogRecord>(csvParserOptions, csvMapper);
-            }
-
-            public IEnumerable<CsvEmailBeaconLogRecord> Parse(string contents)
-            {
-                try
-                {
-                    return _csvParser
-                        .ReadFromString(new CsvReaderOptions(new[] { "\n" }), contents)
-                        .Where(x => x.IsValid)
-                        .Select(x => x.Result)
-                        .ToArray();
-                }
-                catch (Exception)
-                {
-                    return Enumerable.Empty<CsvEmailBeaconLogRecord>();
-                }
-            }
-
-            private class EmailBeaconLogRecordMapping : CsvMapping<CsvEmailBeaconLogRecord>
-            {
-                public EmailBeaconLogRecordMapping() : base()
-                {
-                    base.MapProperty(0, x => x.Date);
-                    base.MapProperty(1, x => x.Time);
-                    base.MapProperty(10, x => x.UserAgent);
-                }
-            }
+                ArrayPool<CloudFrontRecord>.Shared.Return(newData, clearArray: true);
+            }  
         }
     }
 }
