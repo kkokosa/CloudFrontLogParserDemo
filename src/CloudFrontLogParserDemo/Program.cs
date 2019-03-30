cc07fc631328d7faef8546e5dca8ea0a2cd13f51
Steve Gordon <sgordon@hotmail.co.uk>
2019-03-30 11:39:19 +0000

    Initial commit

---
+++ src/CloudFrontLogParserDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+
+namespace CloudFrontLogParserDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var directoryPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+            var filePath = Path.Combine(directoryPath, "sample-cloudfront-access-logs.gz");
+
+            if (File.Exists(filePath))
+            {
+                using (FileStream fs = File.OpenRead(filePath))
+                using (GZipStream decompressionStream = new GZipStream(fs, CompressionMode.Decompress))
+                {
+
+                }
+            }
+        }
+    }
+}
